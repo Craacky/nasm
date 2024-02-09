a42a97958aac2d0ab089bc1bbebe7c9038b51998
Craacky <cbxj.ysa@gmail.com>
2024-02-09 13:15:23 +0300

    *wip

--- lab5/sum-sub.s
+++ lab5/sum-sub.s
@@ -1,8 +1,8 @@
-.global sum
 
-sum:	mov r3, #0
-loop:	ldrb r2, [r0], #1
-	add r3, r2, r3
-	cmp r0, r1
-	bne loop
-
+	.global sum
+sum: 	mov r3, #0 		@ r3 = 0
+loop: 	ldrb r2, [r0], #1 	@ r2 = *r0++; Загрузка элемента массива
+	add r3, r2, r3 		@ r3 += r2; Вычисление суммы
+	cmp r0, r1 		@ if(r0 != r1); Проверка на конец массива
+	bne loop 		@ Цикл, аналог «goto loop» архитектуры х86
+	mov pc, lr 	
