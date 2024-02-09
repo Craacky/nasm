71984c09ced1cdb759f61252294b7a39c3547aa9
Craacky <cbxj.ysa@gmail.com>
2024-02-09 13:02:55 +0300

    *wip

--- lab5/main.s
+++ lab5/main.s
@@ -1,11 +1,11 @@
 .text
 	b start
-	arr: .byte 5,10,15
-	eoa:
-.align
-	start:
+	arr: .byte 5, 10, 15, 20, 25, 30
+	eoa: .align
+	
+	start: 
 	ldr r0, =arr
 	ldr r1, =eoa
 	bl sum
-	swi 0
+	
 	stop: b stop
